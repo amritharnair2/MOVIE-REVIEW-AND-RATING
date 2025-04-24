d55fcb228744a4aa67328ef3d039fa657ad1495a
Amritha <amritharnair2@gmail.com>
2025-04-24 13:09:36 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -11,40 +11,34 @@ require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cookieparser())
-// app.use(cors({
-//         origin: 'http://localhost:5173',
-//         credentials: true
-// }))
 
 const allowedOrigins = [
-        'https://movie-review-and-rating-rafk.vercel.app',
-        'http://localhost:5173'
+  'https://movie-review-and-rating-rafk.vercel.app',
+  'http://localhost:5173'
+];
 
-      ];
-      
-      app.use(cors({
-        origin: function(origin, callback) {
-          if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-          } else {
-            callback(new Error('Not allowed by CORS'));
-          }
-        },
-        credentials: true, 
-      }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 
 dbConnection()
 
 app.get("/", (req, res) => [
-        res.json("Server started")
-    ])
-    
-    
-    app.use("/user", userRouter)
-    app.use("/movie", movieRouter)
-    app.use("/review", reviewRouter)
-    app.use("/admin", adminRouter)
+  res.json("Server started")
+])
+
+app.use("/user", userRouter)
+app.use("/movie", movieRouter)
+app.use("/review", reviewRouter)
+app.use("/admin", adminRouter)
 
 app.listen(process.env.PORT, () => {
-        console.log(`server starts at port ${process.env.PORT}`)
+  console.log(`server starts at port ${process.env.PORT}`)
 })
