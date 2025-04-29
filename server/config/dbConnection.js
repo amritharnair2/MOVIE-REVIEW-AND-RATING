831c68656c5c25588e943f4b54ebcfd5cec8cdc5
Amritha <amritharnair2@gmail.com>
2025-04-29 23:22:30 +0530

    final commit

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        
         console.log("Database connection successful")
     } catch (error) {
         console.log(error)
